6910f73b2b9ba83072aa02e77aea86afeb739b87
jb <sam.rupprechter@gmail.com>
2025-01-19 14:44:16 +0100

    update

--- run_once_zsh_setup.sh
+++ run_once_zsh_setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+
 if [ $USER = 'vscode' ]; then
     sudo apt-get install -y zsh fonts-powerline
     chsh -s $(which zsh)
+
+    # alternative for fonts-powerline:
+    # git clone https://github.com/powerline/fonts.git --depth=1
+    # cd fonts
+    # ./install.sh
+    # cd ..
+    # rm -rf fonts
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# alternative:
-# sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "${HOME}/.oh-my-zsh/" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# alternative for fonts-powerline:
-# git clone https://github.com/powerline/fonts.git --depth=1
-# cd fonts
-# ./install.sh
-# cd ..
-# rm -rf fonts
+    # alternative:
+    # sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Skipping oh-my-zsh installation ('$HOME.oh-my-zsh/' already exists)"
+fi
 
